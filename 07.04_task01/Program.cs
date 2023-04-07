3a573d3a036941c9933f6f8c22312624c93071be
mad rider <mad-ride.rider@yandex.ru>
2023-04-07 14:44:07 +0300

    task 01

--- 07.04_task01/Program.cs
+++ 07.04_task01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int number2sqere = number2 * number2;
+if ( number2sqere == number1 ) 
+    Console.WriteLine("все верно") ;
+else Console.WriteLine("кажется, где то ошибка");
