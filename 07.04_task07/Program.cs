0d76ef730bbafe44742444ac6a9638f6c9888def
mad rider <mad-ride.rider@yandex.ru>
2023-04-07 15:23:15 +0300

    task 7 version1

---
+++ 07.04_task07/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+    number = -number;
+if (number >= 100 && number <= 999)
+    Console.WriteLine(number % 10);
+else
+    Console.WriteLine("Вы ввели не трёхзначное число");
