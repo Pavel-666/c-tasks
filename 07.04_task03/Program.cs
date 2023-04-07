3fb1ccd59e5de3c0188fa54cf72e9ac6e617a8c1
mad rider <mad-ride.rider@yandex.ru>
2023-04-07 14:56:53 +0300

    task version1

--- 07.04_task03/Program.cs
+++ 07.04_task03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Сonsole.WriteLine("Введите порядковый номер дня недели");
+int NumberDay = Convert.ToInt32(Console.ReadLine());
+
+if (NumberDay == 1)
+    Console.WriteLine("понедельник");
+if (NumberDay == 2)
+    Console.WriteLine("вторник");
+if (NumberDay == 3)
+    Console.WriteLine("среда");
+if (NumberDay == 4)
+    Console.WriteLine("четверг");
+if (NumberDay == 5)
+    Console.WriteLine("пятница");
+if (NumberDay == 6)
+    Console.WriteLine("суббота");
+if (NumberDay == 7)
+    Console.WriteLine("воскресение");
+else Console.WriteLine("такого дня недели нет");
