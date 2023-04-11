7b4a282fb88ccca97ab05b51e620244d55f1de3c
mad rider <mad-ride.rider@yandex.ru>
2023-04-11 13:30:47 +0300

    task 06 version 1.0

--- 07.04_task06/Program.cs
+++ 07.04_task06/Program.cs
@@ -9,4 +9,4 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if ( number % 2 == 0)
     Console.WriteLine("число четное");
-else Console.WriteLine("число нечетное");
+else Console.WriteLine("число не является четным");
